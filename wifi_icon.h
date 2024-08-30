d5bdb7e956b42e8aedd9201692a1f0ce23ff91e4
Armin Gorji <arvage@yahoo.com>
2024-08-30 07:50:43 -0700

    Added supporting files

---
+++ wifi_icon.h
@@ -0,0 +1,14 @@
+const char * wifi_icon(float wifi_strength) {
+    if (isnan(wifi_strength))
+			return "\U000F092B"; // No-wifi
+	else if (wifi_strength < 10)
+			return "\U000F0920";  // low-wifi
+	else if (wifi_strength < 30)
+			return "\U000F091F"; // wifi-1
+	else if (wifi_strength < 50)
+			return "\U000F0922"; // wifi-2
+	else if (wifi_strength < 75)
+			return "\U000F0925"; // wifi-3
+	else
+			return "\U000F0928"; // wifi-4
+}
